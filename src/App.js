251423f94278c7e948bf3b90a54dd5d9d990aa9b
Daniel <danielbezerrakmx@gmail.com>
2021-05-25 21:40:58 -0300

    facebook login 2

--- src/App.js
+++ src/App.js
@@ -1,15 +1,41 @@
 import FacebookLogin from "react-facebook-login";
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [logged, setLogged] = useState(false);
+  const [data, setData] = useState({
+    name: "",
+    email: "",
+    picture: {
+      data: {
+        url: "",
+      },
+    },
+  });
   return (
-    <FacebookLogin
-      appId="4078511625548053"
-      autoLoad={true}
-      fields="name,email,picture"
-      onClick={() => console.log("CLICKED")}
-      callback={(res) => console.log("FACEBOOK DATA", res)}
-    />
+    <div>
+      {!logged && (
+        <FacebookLogin
+          appId="4078511625548053"
+          autoLoad={true}
+          fields="name,email,picture"
+          onClick={() => console.log("CLICKED")}
+          callback={(res) => {
+            console.log(res);
+
+            setLogged(true);
+            setData(res);
+          }}
+        />
+      )}
+      {logged && (
+        <div>
+          <h1>nome: {data.name}</h1>
+          <h1>email: {data.email}</h1>
+          <img src={data.picture.data.url} />
+        </div>
+      )}
+    </div>
   );
 }
 
